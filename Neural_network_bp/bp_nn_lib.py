510d378eeeae65c5a160ca16b0c1c7d8c0ac7909
Steven Wang <shangzhouwang@hotmail.com>
2018-03-25 23:28:55 +1100

    start coding the bp step by step

---
+++ Neural_network_bp/bp_nn_lib.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 25 23:02:51 2018
+
+@author: shang
+summary:
+"""
+
+# nerual network class definition
+class nerualNetwork:
+    
+    # initializer
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        
+        ## initialize learning rate
+        self.lr = learningrate
+        pass
+    
+    # train the nn model
+    def train():
+        pass
+    
+    # query the nerual network
+    
+    def query():
+        pass
+    
+    
